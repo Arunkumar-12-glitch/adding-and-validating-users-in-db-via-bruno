48b6e1f8d150b03e21afe1b3f56538afb48247e0
Arunkumar-12-glitch <arunkumar227264@gmail.com>
2025-04-07 11:00:51 +0530

    changes

--- index.js
+++ index.js
@@ -1,15 +1,51 @@
 const express = require('express');
-const { resolve } = require('path');
+const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
+const User = require('./models/user');
+const dotenv=require('dotenv')
+
+dotenv.config();
+const PORT=process.env.PORT ||6000;
 
 const app = express();
-const port = 3010;
+app.use(express.json()); // for parsing application/json
 
-app.use(express.static('static'));
 
-app.get('/', (req, res) => {
-  res.sendFile(resolve(__dirname, 'pages/index.html'));
+mongoose
+.connect(process.env.MONGO_URI)
+.then(() => console.log('MongoDB connected successfully'))
+.catch(err => console.log(err));
+
+
+app.post('/register', async (req, res) => {
+  const { username, email, password } = req.body;
+
+ 
+  if (!username || !email || !password) {
+    return res.status(400).json({ message: 'All fields are required' });
+  }
+
+  try {
+    
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    
+    const newUser = new User({
+      username,
+      email,
+      password: hashedPassword
+    });
+
+   
+    await newUser.save();
+
+    res.status(201).json({ message: 'User registered successfully' });
+
+  } catch (err) {
+    res.status(500).json({ message: 'Server error', error: err.message });
+  }
 });
 
-app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`);
+app.listen(3000, () => {
+  console.log('Server running on http://localhost:3000');
 });
